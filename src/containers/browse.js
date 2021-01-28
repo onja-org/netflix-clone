e855a5c985fff9882f25bf12c86a84809dd17b5d
pinois <pinois@mc.local>
2021-01-28 12:04:47 +0300

    updated protected routes

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Fuse from 'fuse.js';
-import { Header, Loading, Card } from '../components';
+import { Header, Loading, Card, Player } from '../components';
 import * as ROUTES from '../constants/routes';
 import { FirebaseContext } from '../context/firebase';
 import SelectProfileContainer from './profiles';
@@ -117,7 +117,12 @@ export default function BrowseContainer({ slides }) {
 								</Card.Item>
 							))}
 						</Card.Entities>
-						<Card.Feature category={category}></Card.Feature>
+						<Card.Feature category={category}>
+							<Player>
+								<Player.Button />
+								<Player.Video />
+							</Player>
+						</Card.Feature>
 					</Card>
 				))}
 			</Card.Group>
